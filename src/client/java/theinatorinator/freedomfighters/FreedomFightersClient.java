0b95537d6ccc824f76d953773d8fcb1061f823a7
Theinatorinator <Logan.mamanakis@gmail.com>
2023-12-24 18:33:15 -0700

    initial commit

---
+++ src/client/java/theinatorinator/freedomfighters/FreedomFightersClient.java
@@ -0,0 +1,10 @@
+package theinatorinator.freedomfighters;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FreedomFightersClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
